98862d2e92246f7fcd2f4dd367bc29936fe35816
mahbubcrx12 <mahbubul190@gmail.com>
2023-03-29 16:03:27 +0600

    adding notification

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:task_management/service/notification_services.dart';
 import 'package:task_management/service/theme_services.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,11 +11,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return  SafeArea(
         child:Scaffold(
-          appBar: _appBar(),
+          appBar: _appBar(
+
+          ),
           body: Column(children: [
             Text("theme",style: TextStyle(
               fontSize: 30,
@@ -29,6 +43,11 @@ class _HomePageState extends State<HomePage> {
       leading: GestureDetector(
         onTap: (){
             ThemeService().switchTheme();
+            notifyHelper.displayNotification(
+              title: "Theme Changed",
+              body:Get.isDarkMode ? "Activated light Theme" : "Activated dark theme"
+            );
+            notifyHelper.scheduledNotification();
         },
         child: Icon(Icons.nightlight_round,
         size: 20,),
